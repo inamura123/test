75ac22cb40753e3047e7c576d6efb4c3d85508fa
inamura123 <89825668+inamura123@users.noreply.github.com>
2021-08-31 13:46:08 +0900

    Add files via upload

---
+++ ITプロジェクト用/index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>LearCoDe</title>
+</head>
+
+<body>
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#Home">
+            <h1>LearCoDe</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">ホーム</a></li>
+            <li><a href="#projects" data-after="Projects">レッスン一覧</a></li>
+            <li><a href="#contact" data-after="Contact">ログイン</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>年齢関係なく学べる<span></span></h1>
+        <h1>LearCoDe <span></span></h1>
+        <a href="#projects" type="button" class="cta">Resson</a>
+      </div>
+    </div>
+  </section>
+
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">プログラミングを学ぶメリット</h1>
+        <p>この十数年で、書類作成やファイル管理など、仕事でパソコンを利用するのは当たり前になりました。同じように、誰もがプログラミングの知識を持つことが当然の時代がすぐにやってきます。最近では、家電製品や自動車など、身の回りの製品にインターネット通信機能を持たせた「Internet of Things」(IoT)という概念も登場しています。遠隔操作などの制御処理を開発する機会も増え、ますますプログラミングスキルを持つ人材の需要は増すばかりです。</p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <h2>柔軟なスタイルで働きやすい</h2>
+          <p>ログラミングは当然ながら、基本的にパソコンを使った仕事がメインとなります。パソコンとスキルさえあれば仕事できるので、勤務場所や勤務時間にとらわれず働きやすいのが大きなメリットです。ノートパソコンがあればカフェのような場所でも働くことが可能です。通勤が不要になる分時間を有効活用できるため、家事や趣味などもはかどります。</p>
+        </div>
+        <div class="service-item">
+          <h2>アイデアをプログラミングで形にできる</h2>
+          <p>プログラミングを習得すればWebサービスやスマホアプリなど、さまざまなものが作れるようになります。特別に難しいプログラミング技術を使わずとも、1つのアイデアが多くの人に愛されるサービスへと変わることも。
+有名なレシピサイトの「クックパッド」は、「レシピをオンラインで共有する」というアイデアから生み出されたもの。</p>
+        </div>
+        <div class="service-item">
+          <h2>起業・独立がしやすい</h2>
+          <p>スキルがあるとアイデアから新しいものを生み出しやすくなります。自分自身で新しいビジネスのきっかけを作れるため、起業や独立という選択肢がより身近になるでしょう。
+            独立すれば高い自由度で仕事できますし、起業して成功すれば大幅な収入アップのチャンスもあります。このようにプログラマーは起業や独立の敷居が低く、新しい場所に踏み出しやすいのが強みといえるでしょう。</p>
+        </div>
+        <div class="service-item">
+          <h2>将来性の高い分野で働ける</h2>
+          <p>IT業界では、AIやIoTなどの新しい技術が次々と生み出されています。プログラミングは、このような新しい技術に関わりやすいのも魅力のひとつです。
+またAIやIoTは業務効率化といった企業にとってのメリットが大きいため、今後も需要が高まっていくと思われます。将来性の高い分野のため、仕事がなくなる心配をする必要がないのは大きいと思います。</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">レッスン一覧</h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>HTML</h1>
+            <p>HTML（エイチティーエムエル、HyperText Markup Language）は、ウェブページを作成するために開発された言語です。 現在、インターネット上で公開されてるウェブページのほとんどは、HTMLで作成されています。
+HyperText Markup Languageを日本語で表すなら、「ハイパーテキストに目印をつける言語」くらいの意味になります。 ハイパーテキスト（HyperText）とは、ハイパーリンクを埋め込むことができる高機能なテキストです。 ハイパーリンクというのは、ウェブページで下線の付いたテキストなどをクリックすると別ページへ移動する、あのリンクのことです。
+ハイパーテキストでは、ウェブページから別のウェブページにリンクをはったり、 ウェブページ内に画像・動画・音声などのデータファイルをリンクで埋め込むことができます。 HTMLには、このハイパーリンク機能で関連する情報同士を結びつけて、情報を整理するという特徴があります。</p>
+          </div>
+          <div class="project-img">
+            <img src="/img/HTML.png">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>CSS</h1>
+            <p>CSS（Cascading Style Sheets、カスケーディング・スタイル・シート）とは、ウェブページのスタイルを指定するための言語です。 ワープロソフトなどで作成される文書も含めて、文書のスタイルを指定する技術全般をスタイルシートといいます。 HTMLやXHTMLなどで作成されるウェブページにスタイルを適用する場合には、スタイルシート言語の1つであるCSSが一般的に利用されています。ウェブページがスクリーンに表示される際の色・サイズ・レイアウトなどの表示スタイルや、 プリンタなどの機器で印刷・出力される際の出力スタイル、 音声で読み上げられる際の再生スタイルなど、 ウェブページをどのようなスタイルで表示・出力・再生するかについて指定することができます。</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/css3.png">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>JavaScript</h1>
+            <p>JavaScriptは、Webページを構成する全てのオブジェクトを操作できるため、魔法のようなことを実行できます。ユーザーが適切な形式でメールアドレスをフォームに入力したかどうかをチェックする。
+Google.comのページをロードするとき、テキストカーソルを検索ボックスに自動で出現させる。
+Webページ全体をリロードしなくても、一部分だけロードすることを可能とするので、例えば映画のWebサイトで、次回のシリーズのリスト表示の部分だけロードしたいときなどに役立つ。</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/js.png">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>PHP</h1>
+              <p>PHPとは動的にWebページを生成することができるサーバーサイドのスクリプト言語です。他のプログラミング言語と比較して仕様や文法が簡単なため習得しやすいと言われています。またMySQLなどのデータベースとの連携が容易なことなどから、WordPressを含めたWebアプリケーションの開発にもよく使われる有名なスクリプト言語でもあります。HPが動的にWebページを生成するのに対してHTMLは静的にWebページの表示を行います。「静的なWebページ」とはいつ誰が見ても同じように表示されるページで、一方の「動的なWebページ」とはアクセスしたタイミングや状況によって表示内容が変わるページのことです。たとえば、PHPがよく使われる掲示板はユーザーによる発言が増える度に表示される内容が変わりますよね。これが「動的なWebページ」の例です。PHPによって動的にHTMLを生成して表示することが可能です。</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/php.png">
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1>LearCoDe</h1>
+      </div>
+      <h2>Your Complete Web Solution</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      </div>
+      <p>Copyright © 2021 LearCoDe. All rights reserved</p>
+    </div>
+  </section>
+
+  <script src="./app.js"></script>
+</body>
+
+</html>
